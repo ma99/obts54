4db3bc06bedeb4dcef94cc1488287d1b3040aa93
mas <ma99.dev@gmail.com>
2017-04-30 13:03:25 +0300

    add text-danger for error txt

--- resources/views/includes/guest.blade.php
+++ resources/views/includes/guest.blade.php
@@ -5,7 +5,7 @@
 	    <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
 	    <input id="name" type="text" class="form-control" name="name" v-model="form.name">
 	</div>
-	<span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+	<span class="help text-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
 	</div>
 
 	<div class="form-group">
@@ -14,7 +14,7 @@
 	    <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
 	    <input id="email" type="email" class="form-control" name="email" v-model="form.email">
 	</div>
-	<span class="help is-danger" v-if="form.errors.has('email')" v-text="form.errors.get('email')"></span>
+	<span class="help text-danger" v-if="form.errors.has('email')" v-text="form.errors.get('email')"></span>
 	</div>
 
 	<div class="form-group">
@@ -23,7 +23,7 @@
 	    <span class="input-group-addon"><i class="fa fa-mobile fa-fw"></i></span>
 	    <input id="phone" type="text" class="form-control" name="phone" v-model="form.phone">
 	</div>
-	<span class="help is-danger" v-if="form.errors.has('phone')" v-text="form.errors.get('phone')"></span>
+	<span class="help text-danger" v-if="form.errors.has('phone')" v-text="form.errors.get('phone')"></span>
 	</div>
 	<button v-show="isSeatSelected" class="btn btn-primary" :disabled="form.errors.any()">Continue</button>                                      
 </form>
