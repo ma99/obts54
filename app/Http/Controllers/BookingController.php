741faa722d5bf94d75dd926f917ce2610cfd9d30
mas <ma99.dev@gmail.com>
2017-05-15 12:30:18 +0300

    code refactoring

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -135,7 +135,7 @@ class BookingController extends Controller
         // user/ stuff/ guest
         // For Registered USER
         //(Auth::check()
-        if ( auth()->check() ) {
+        //if ( auth()->check() ) {
             /*
             $scheduleId = $this->request->input('schedule_id'); 
             $totalSeats = $this->request->input('total_seats'); 
@@ -188,96 +188,96 @@ class BookingController extends Controller
                 'dropping_point' => 'BBB',
             ]);*/
 
-        }
+       // } auth chk end if
         
         //**** For GUEST user      
 
-            $this->validate($this->request, [
-                'name' => 'required',
-                "email" => 'required',
-                "phone" => 'required',            
-            ]); 
-            //$busId = $this->request->input('bus_id'); 
-            $name = $this->request->input('name');
-            $email = $this->request->input('email');
-            $phone = $this->request->input('phone');
-            // $scheduleId = $this->request->input('booking_id'); 
-            $scheduleId = $this->request->input('schedule_id'); 
-            $totalSeats = $this->request->input('total_seats'); 
-            $totalFare = (float) $this->request->input('total_fare'); 
-            $date = $travelDate = $this->request->input('date');
-            $selectedSeats = $this->request->input('selected_seats'); //
+        //     $this->validate($this->request, [
+        //         'name' => 'required',
+        //         "email" => 'required',
+        //         "phone" => 'required',            
+        //     ]); 
+        //     //$busId = $this->request->input('bus_id'); 
+        //     $name = $this->request->input('name');
+        //     $email = $this->request->input('email');
+        //     $phone = $this->request->input('phone');
+        //     // $scheduleId = $this->request->input('booking_id'); 
+        //     $scheduleId = $this->request->input('schedule_id'); 
+        //     $totalSeats = $this->request->input('total_seats'); 
+        //     $totalFare = (float) $this->request->input('total_fare'); 
+        //     $date = $travelDate = $this->request->input('date');
+        //     $selectedSeats = $this->request->input('selected_seats'); //
 
-            $dt = date_format(date_create($date),"Ymd");        
-            $bookingId = strtoupper(bin2hex(random_bytes(4)));            
+        //     $dt = date_format(date_create($date),"Ymd");        
+        //     $bookingId = strtoupper(bin2hex(random_bytes(4)));            
 
-            $date = date("Y-m-d", strtotime($date));            
-            //return 'success';
+        //     $date = date("Y-m-d", strtotime($date));            
+        //     //return 'success';
 
-            // Storing Guest User Info             
-            $guestUserIsAvailable = GuestUser::where('phone', $phone)->first();  // user available or not in guest_user table
-            if ( $guestUserIsAvailable ) {
-                $guestUserIsAvailable->delete();
-                GuestUser::create([
-                    'name' => $name,
-                    'email' => $email,
-                    'phone' => $phone, 
-                ]);
-            }
-            else {
-                GuestUser::create([
-                    'name' => $name,
-                    'email' => $email,
-                    'phone' => $phone,
-                ]);
-            }
+        //     // Storing Guest User Info             
+        //     $guestUserIsAvailable = GuestUser::where('phone', $phone)->first();  // user available or not in guest_user table
+        //     if ( $guestUserIsAvailable ) {
+        //         $guestUserIsAvailable->delete();
+        //         GuestUser::create([
+        //             'name' => $name,
+        //             'email' => $email,
+        //             'phone' => $phone, 
+        //         ]);
+        //     }
+        //     else {
+        //         GuestUser::create([
+        //             'name' => $name,
+        //             'email' => $email,
+        //             'phone' => $phone,
+        //         ]);
+        //     }
 
             
 
-        //****storing booking info corrosponding guest user ***
-            //GuestUser::bookings()->create([            
-            Booking::create([            
-                'id' => $bookingId,
-                'schedule_id' => $scheduleId,
-                'user_id' => $phone, // guest phn as user id
-                'seats' => $totalSeats,
-                'amount' => $totalFare,
-                'date' => $date,
-                'pickup_point' => 'AAA',
-                'dropping_point' => 'BBB',
-            ]);
+        // //****storing booking info corrosponding guest user ***
+        //     //GuestUser::bookings()->create([            
+        //     Booking::create([            
+        //         'id' => $bookingId,
+        //         'schedule_id' => $scheduleId,
+        //         'user_id' => $phone, // guest phn as user id
+        //         'seats' => $totalSeats,
+        //         'amount' => $totalFare,
+        //         'date' => $date,
+        //         'pickup_point' => 'AAA',
+        //         'dropping_point' => 'BBB',
+        //     ]);
 
            
-            /** User + Guest User **/
+        //     /** User + Guest User **/
 
-            $selectedSeats = json_decode(json_encode($selectedSeats), FALSE); // array to object
-            $seatNo = '';
-            foreach ($selectedSeats as $seat ) {
-               $seatNo = $seatNo .' '. $seat->seat_no;
-               Seat::create([
-                    'booking_id' => $bookingId,
-                    'seat_no' => $seat->seat_no,
-                    'status' => $seat->status,
-                    ]);
-                // fire broadcast event 
-                broadcast(new SeatStatusUpdatedEvent($seat, $scheduleId, $travelDate))->toOthers();
-            }
-            //return $seatNo;
-            $seats = trim($seatNo);
+        //     $selectedSeats = json_decode(json_encode($selectedSeats), FALSE); // array to object
+        //     $seatNo = '';
+        //     foreach ($selectedSeats as $seat ) {
+        //        $seatNo = $seatNo .' '. $seat->seat_no;
+        //        Seat::create([
+        //             'booking_id' => $bookingId,
+        //             'seat_no' => $seat->seat_no,
+        //             'status' => $seat->status,
+        //             ]);
+        //         // fire broadcast event 
+        //         broadcast(new SeatStatusUpdatedEvent($seat, $scheduleId, $travelDate))->toOthers();
+        //     }
+        //     //return $seatNo;
+        //     $seats = trim($seatNo);
 
-            return response()->json([
-                'name' => $name,
-                'email' => $email,
-                'phone' => $phone,           
-                'booking_id' => $bookingId,
-                //'schedule_id' => $scheduleId,
-                'seats' => $totalSeats,
-                'seat_no' => $seats,
-                'amount' => $totalFare,
-                'date' => $date,
-                'pickup_point' => 'AAA',
-                'dropping_point' => 'BBB',
-            ]);
+        //     return response()->json([
+        //         'name' => $name,
+        //         'email' => $email,
+        //         'phone' => $phone,           
+        //         'booking_id' => $bookingId,
+        //         //'schedule_id' => $scheduleId,
+        //         'seats' => $totalSeats,
+        //         'seat_no' => $seats,
+        //         'amount' => $totalFare,
+        //         'date' => $date,
+        //         'pickup_point' => 'AAA',
+        //         'dropping_point' => 'BBB',
+        //     ]);
     	
     }
 
