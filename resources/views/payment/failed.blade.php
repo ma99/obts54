44f4d269f99344ad6d92abc782dadb1b8f94a85d
mas <ma99.dev@gmail.com>
2017-07-06 12:40:59 +0300

    implement failed trx

--- resources/views/payment/failed.blade.php
+++ resources/views/payment/failed.blade.php
@@ -3,5 +3,13 @@
 @section('content')
 <div class="panel body">
 	<h3>Payment failed</h3>
+	<h2> {{ $validation_message }} </h2>
+	<form action="{{ url('/pay/'.$bookingId) }}">
+		 {{ csrf_field() }}
+		 <button type="submit" class="btn btn-info">
+                <i class="fa fa-btn btn-info"></i>Try Again
+         </button>
+		<a href= "{{ url('/cancel/'.$bookingId) }}" type="button" value="Cancel" class="btn btn-warning">Cancel</a>
+	</form>
 </div>
 @endsection
