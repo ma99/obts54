545543399ff8601a94a1a03baca3fdbe1fe9361b
ma99 <ma99.dev@gmail.com>
2017-07-10 22:59:09 +0300

    failed & cancelled view modified

--- resources/views/payment/cancelled.blade.php
+++ resources/views/payment/cancelled.blade.php
@@ -3,34 +3,24 @@
 @section('content')
 	<div class="row">
 	    <div class="col-md-8 col-md-offset-2">
-	    <div class="panel panel-default">
-	      <div class="panel-heading">Payment Cancelled</div>
-	      <div class="panel body">
-			<h3>Payment Cancelled</h3>
-			{{-- <h2> {{ $validation_message }} </h2> --}}
-			
-			<div id="payment">				
-				{{-- <form action="{{ url('/pay/'.$bookingId) }}"> --}}
-				<form action="{{ url('/pay/') }}">
-					 {{ csrf_field() }}
-					 <button type="submit" class="btn btn-info">
-			            Try Again
-			         </button>
-					{{-- <a href= "{{ url('/cancel/'.$bookingId) }}" type="button" value="Cancel" class="btn btn-warning">Cancell</a> --}}
-				</form>
-			
-				{{-- <form action="{{ url('/cancel/'.$bookingId) }}"> --}}
-				<form action="{{ url('/cancel/') }}">
-					 {{ csrf_field() }}
-					 <button type="submit" class="btn btn-warning">
-			            Cancel
-			         </button>		
-				</form>
-			</div>
-			
-		  </div>
-	    </div>
-	  </div>
+		    <div class="panel panel-warning">
+		      <div class="panel-heading">Payment Cancelled</div>
+		      
+		      <div class="panel body">					
+						<h2> {{ $validation_message }} </h2>			
+						<div id="payment">				
+							<form action="{{ url('/pay/'.$bookingId) }}">
+								 {{ csrf_field() }}
+								 <button type="submit" class="btn btn-info">Try Again</button>						
+							</form>
+						
+							<form action="{{ url('/cancel/'.$bookingId) }}">					
+								 {{ csrf_field() }}
+								 <button type="submit" class="btn btn-warning">Cancel</button>		
+							</form>
+						</div>			
+			  	</div>
+		    </div>
+	  	</div>
 	</div>   
-
 @endsection
--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="panel body">
-		{{-- @if (isset($payment))
-			<ul>
-				<li> Ref: {{ $payment->booking_id }} </li>
-				<li> Transaction Ref: {{ $payment->transaction_id }} </li>
-				<li> Amount: {{ $payment->amount }} </li>
-				<li>  Status: {{ $payment->status }} </li>
-			</ul>
-		@endif	 --}}
+	<div class="row">
+	    <div class="col-md-8 col-md-offset-2">
+		    <div class="panel panel-success">		      
+		      <div class="panel-heading">Payment Status</div>		      			      		      
+		      <div class="panel body">
 
-		@if ($payment_status == 'success')
-			<h1> {{ $payment_status }}  </h1>
-			<ul>
-				<li> Status: {{ $status }} </li>
-				<li> Transaction Ref: {{ $tran_id }} </li>
-				<li>  Validation ID: {{ $val_id }}</li>				
-				<li> Amount: {{ $store_amount }}</li>
-				<li> Amount Including Charge: {{ $amount }}</li>
-			</ul>
-		@else
-			<p> {{ $validation_message }}  </p>			
-		@endif	
-	</div>
+		      	@if ($payment_status == 'success')
+							<h1> {{ $payment_status }}  </h1>
+							<ul>
+								<li> Status: {{ $status }} </li>
+								<li> Transaction Ref: {{ $tran_id }} </li>
+								<li>  Validation ID: {{ $val_id }}</li>				
+								<li> Amount: {{ $store_amount }}</li>
+								<li> Amount Including Charge: {{ $amount }}</li>
+							</ul>
+						@else
+							<p> {{ $validation_message }}  </p>			
+						@endif												
+
+			  	</div>
+		    </div>
+	  	</div>
+	</div>   
 @endsection
