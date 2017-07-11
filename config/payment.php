9778c09b98fb5965e8160b5857e391ad42896d97
mas <ma99.dev@gmail.com>
2017-07-11 12:59:48 +0300

    add payment config file

---
+++ config/payment.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'sslcommerz' => [        
+		'sandbox' => true,
+		'store_id' => env('SSLCOMMERZ_STORE_ID'),
+	    'store_password' => env('SSLCOMMERZ_STORE_PASSWORD'),
+	    'sandbox_url' => 'https://sandbox.sslcommerz.com/gwprocess/v3/process.php',       
+	    'live_url' => 'https://securepay.sslcommerz.com/gwprocess/v3/process.php',
+	    'sandbox_validation_url' => 'https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php',   
+	    'live_validation_url' => 'https://securepay.sslcommerz.com/validator/api/validationserverAPI.php'               
+    ],
+];
