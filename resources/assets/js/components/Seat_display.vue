86d5637740514d98bdd9d9d9b4c20e9023c21192
ma99 <ma99.dev@gmail.com>
2017-08-08 20:53:34 +0300

    add pickup & drop

--- resources/assets/js/components/Seat_display.vue
+++ resources/assets/js/components/Seat_display.vue
@@ -675,15 +675,15 @@
   /* The Modal (background) */
   .modal {
       display: block; 
-      position: fixed; /* Stay in place */
-      z-index: 11111; /* Sit on top */
+      position: fixed; //* Stay in place */
+      z-index: 11111; //* Sit on top */
       left: 0;
       top: 0;
-      width: 100%; /* Full width */
-      height: 100%; /* Full height */
-      overflow: auto; /* Enable scroll if needed */
-      background-color: rgb(0,0,0); /* Fallback color */
-      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+      width: 100%; //* Full width */
+      height: 100%; //* Full height */
+      overflow: auto; //* Enable scroll if needed */
+      background-color: rgb(0,0,0); //* Fallback color */
+      background-color: rgba(0,0,0,0.4); //* Black w/ opacity */
   }
   
   /* seat display */
